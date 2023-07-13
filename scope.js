bf6508a7fb8669282caac6ff924b03f37724f3e4
Ryan Carpenter <rccarpenter@outlook.com>
2023-07-13 13:42:08 -0600

    adding notes

--- scope.js
+++ scope.js
@@ -14,3 +14,24 @@ function ask() { // Red Marble (global scope)
 otherClass(); // "Welcome!"
 ask(); // "Why"
 
+
+
+// function declaration - attach their name to the encolsing scope - Red Marble
+function teacher() {}
+
+// function expression - add their marble to their own scope - anotherTeacher = Blue Marble (myTeacher would be a Red Marble)
+var myTeacher = function anotherTeacher() {
+    console.log(anotherTeacher);
+};
+
+// named function expression - should prefer this over the anonymous versions
+var keyHandler = function keyHandler() {}
+
+// anonymous function expression
+var clickHandler = function() {}
+var clickHandler2 = () => {}
+
+// why to use named function expressions
+// 1 - reliable function self-reference (recursion, eventhandler, unbinding, accessing properties, etc)
+// 2 - more debuggable stack traces
+// 3 - makes your code more self-documenting
