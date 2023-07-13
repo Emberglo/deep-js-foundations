98d98ec268b3780011f88f2b4f86f67535c3b094
Ryan Carpenter <rccarpenter@outlook.com>
2023-07-13 14:42:44 -0600

    exercises

--- scope.js
+++ scope.js
@@ -35,3 +35,79 @@ var clickHandler2 = () => {}
 // 1 - reliable function self-reference (recursion, eventhandler, unbinding, accessing properties, etc)
 // 2 - more debuggable stack traces
 // 3 - makes your code more self-documenting
+
+// this:
+var ids = people.map(function getId(person) {return person.id});
+// is better than this:
+var ids = people.map(person => person.id);
+
+// named function definitions 
+//            > 
+//named function expressions 
+//            > 
+//anonymous function expressions
+
+
+// function expressions exercise
+
+function getStudentById(studentId) {
+    return studentRecords.find(function matchId(record) {
+        return (record.id == studentId);
+    })
+}
+
+function printRecords(recordIds) {
+    var records = recordIds.map(getStudentById);
+
+    // can and should probably be extracted to it's own function
+    records.sort(function sortByNameAsc(record1, record2) {
+        if (record1.name < record2.name) {
+            return -1;
+        } else if (record1.name > record2.name) {
+            return 1;
+        } else {
+            return 0;
+        }
+    });
+
+    records.array.forEach(function printRecord(record) {
+        console.log(`${record.name} (${record.id}) : ${record.paid} ? "Paid" : "Not Paid"`)
+    });
+}
+
+function paidStudentsToEnroll() {
+    var idsToEnroll = studentRecords.filter(function needsToEnroll() {
+        return (record.paid && !currentEnrollments.includes())
+    })
+    .map(function getStudentId(record) {
+        return record.id;
+    });
+
+    return [...currentEnrollment, ...idsToEnroll];
+}
+
+function remindUnpaid(recordIds) {
+    var unpaidIds = recordIds.filter(function isUnpaid(studentId) {
+        var record = getStudentById(studentId);
+        return !record.paid;
+    });
+
+    printRecords(unpaidIds);
+}
+
+//same functionality as above but written with anonymous/arrow functions
+
+var getStudentById = studentId => studentRecords.find(record => record.id == studentId);
+
+var printRecords = recordIds => 
+    recordIds.map(getStudentById)
+    .sort((record1, record2) => (record1.name < record2.name) ? -1 : (record1.name > record2.name) ? 1 : 0)
+    .forEach(record => console.log(`${record.name} (${record.id}) : ${record.paid} ? "Paid" : "Not Paid"`));
+
+var paidStudentsToEnroll = () => [
+    ...currentEnrollment,
+    ...(studentRecords.filter(record => record.paid && !currentEnrollments.includes())).map(record => record.id)
+];
+
+var remindUnpaid = recordIds => 
+    printRecords(recordIds.filter(studentId => !getStudentById().paid));
